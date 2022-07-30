34660dfabb00ba2f571874fe060e59f0f5a8bf77
juan gore <juangorecki@gmail.com>
2022-07-29 23:49:08 -0300

    funcionamiento de formulario

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+require('dotenv').config();
+
 var indexRouter = require('./routes/index');
 var especialidadesRouter = require('./routes/especialidades')
 var trabajosRouter = require('./routes/trabajos')
--- routes/contactanos.js
+++ routes/contactanos.js
@@ -1,10 +1,44 @@
 var express = require('express');
 var router = express.Router();
+var nodemailer = require('nodemailer');
 
 router.get('/', function(req,res,next){
     res.render('contactanos',{
         isContactanos: true
     });
 });
+
+router.post('/', async function(req,res,next){
+    //console.log(req.body)
+    var nombre = req.body.nombre;
+    var email = req.body.email;
+    var tel = req.body.tel;
+    var comentarios = req.body.comentarios;
+
+    var obj = {
+        to:'juangorecki@gmail.com',
+        subject:'Contacto desde la pagina web',
+        html: nombre + " contacto a traves de la pagina web, este es su correo: " + email + ".<br> Su tel es: " + tel + " Este es su mensaje: " + comentarios
+
+    }
+    var transport = nodemailer.createTransport({
+        host: process.env.SMTP_HOST,
+  port: process.env.SMTP_PORT,
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS
+  }
+
+    })
+
+    var info = await transport.sendMail(obj);
+    res.render('contactanos',{
+        message: 'Formulario enviado',
+        isContactanos: true
+    })
+
+});
+
+
 
 module.exports = router; 
